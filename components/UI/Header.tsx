1756520283e68eeec1d9f0f39010be30db0644fb
Franco Picco <francoapicco@gmail.com>
2026-01-02 17:21:32 -0300

    Header Basic UI

--- components/UI/Header.tsx
+++ components/UI/Header.tsx
@@ -1,13 +1,19 @@
 import SignOutButton from "../shared/SignOutButton";
+import UserIcon from "./Icons/User";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center mb-10">
-      <div>
-        <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
-        <p className="text-zinc-400 text-sm">User</p>
+    <header className="flex place-content-center p-5">
+      <div className="w-5/6 flex justify-between">
+        <div className="flex items-center gap-3">
+          <h1 className="text-2xl font-bold tracking-tight">Flywheel</h1>
+        </div>
+        <div className="flex place-content-center gap-4">
+          <UserIcon />
+          <span className="text-zinc-400 text-sm">Username</span>
+          <SignOutButton />
+        </div>
       </div>
-      <SignOutButton />
     </header>
   );
 }
